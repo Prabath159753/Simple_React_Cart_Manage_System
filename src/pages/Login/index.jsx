5c738247708db543ac72c9a7d38081b37ed51d1b
Kavishka <kavishkaprabath21@gmail.com>
2022-08-09 15:50:35 +0530

    add Cart, Dashboard, Login, Products and User pages

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    render() { 
+        return (
+            <h1>Login Screen</h1>
+        );
+    }
+}
+ 
+export default Login;
---
+++ src/pages/Products/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Products extends Component {
+    render() { 
+        return (
+            <h1>Products Screen</h1>
+        );
+    }
+}
+ 
+export default Products;
