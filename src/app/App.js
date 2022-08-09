273ceaca2a6c582a7d7b1ca56f2d8ed508dbc006
Kavishka <kavishkaprabath21@gmail.com>
2022-08-09 15:53:33 +0530

    add axios.js file and update App.js file

--- src/app/App.js
+++ src/app/App.js
@@ -3,13 +3,22 @@ import { Routes, Route } from "react-router-dom";
 
 // Sub Outlets (Outlet mean sub children of Layout)
 import Login from "../pages/Login";
-
+import Dashboard from "../pages/Dashboard";
+import User from "../pages/User";
+import Products from "../pages/Products";
+import Cart from "../pages/Cart";
+// import NotFound from "../pages/NotFound";
 
 function App() {
     return (
     
         <Routes>
             <Route exact path="/" element={<Login/>}/>
+            <Route path="signup" element={<User/>}/>
+            <Route path="dashboard" element={<Dashboard/>}/>
+            <Route path="product" element={<Products/>}/>
+            <Route path="cart" element={<Cart/>}/>
+            {/* <Route path="*" element={<NotFound/>}/> */}
         </Routes>
     
     );
